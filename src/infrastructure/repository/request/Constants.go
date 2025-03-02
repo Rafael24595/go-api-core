ee68a2f953fa4da3a07a223948784f7ae7fc4627
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-03-03 00:06:38 +0100

    [FEATURE] Memory repository default path constants updated.

--- src/infrastructure/repository/request/Constants.go
+++ src/infrastructure/repository/request/Constants.go
@@ -1,8 +1,8 @@
 package request
 
 const(
-	CSVT_HISTORIC_FILE_PATH string = "./db/collection_request_historic.csvt"
-	CSVT_PERSISTED_FILE_PATH string = "./db/collection_request_persisted.csvt"
+	CSVT_FILE_PATH_REQUEST string = "./db/table_request.csvt"
+	CSVT_FILE_PATH_RESPONSE string = "./db/table_response.csvt"
 	
 	JSON_HISTORIC_FILE_PATH string = "./db/collection_request_historic.json"
 	JSON_PERSISTED_FILE_PATH string = "./db/collection_request_persisted.json"
