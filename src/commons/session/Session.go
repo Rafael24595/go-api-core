62ac5bcc11ecc0309faab29bc415c505f4b33246
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-04-24 19:09:11 +0200

    [FEATURE] Update session definition:
    - Add fields IsProtected, IsAdmin and Count.

--- src/commons/session/Session.go
+++ src/commons/session/Session.go
@@ -1,8 +1,11 @@
 package session
 
 type Session struct {
-	Username  string `json:"username"`
-	Secret    []byte `json:"secret"`
-	Timestamp int64  `json:"timestamp"`
-	Context   string `json:"context"`
+	Username    string `json:"username"`
+	Secret      []byte `json:"secret"`
+	Timestamp   int64  `json:"timestamp"`
+	Context     string `json:"context"`
+	IsProtected bool   `json:"is_protected"`
+	IsAdmin     bool   `json:"is_admin"`
+	Count       int    `json:"count"`
 }
--- src/infrastructure/dto/DtoSession.go
+++ src/infrastructure/dto/DtoSession.go
@@ -3,10 +3,13 @@ package dto
 import "github.com/Rafael24595/go-api-core/src/commons/session"
 
 type DtoSession struct {
-	Username  string `json:"username"`
-	Secret    string `json:"secret"`
-	Timestamp int64  `json:"timestamp"`
-	Context   string `json:"context"`
+	Username    string `json:"username"`
+	Secret      string `json:"secret"`
+	Timestamp   int64  `json:"timestamp"`
+	Context     string `json:"context"`
+	IsProtected bool   `json:"is_protected"`
+	IsAdmin     bool   `json:"is_admin"`
+	Count       int    `json:"count"`
 }
 
 func NewDtoSessionDefault() *DtoSession {
@@ -19,18 +22,24 @@ func (s DtoSession) PersistenceId() string {
 
 func ToSession(dto DtoSession) *session.Session {
 	return &session.Session{
-		Username: dto.Username,
-		Secret: []byte(dto.Secret),
-		Timestamp: dto.Timestamp,
-		Context: dto.Context,
+		Username:    dto.Username,
+		Secret:      []byte(dto.Secret),
+		Timestamp:   dto.Timestamp,
+		Context:     dto.Context,
+		IsProtected: dto.IsProtected,
+		IsAdmin:     dto.IsAdmin,
+		Count:       dto.Count,
 	}
 }
 
 func FromSession(session session.Session) *DtoSession {
 	return &DtoSession{
-		Username: session.Username,
-		Secret: string(session.Secret),
-		Timestamp: session.Timestamp,
-		Context: session.Context,
+		Username:    session.Username,
+		Secret:      string(session.Secret),
+		Timestamp:   session.Timestamp,
+		Context:     session.Context,
+		IsProtected: session.IsProtected,
+		IsAdmin:     session.IsAdmin,
+		Count:       session.Count,
 	}
 }
