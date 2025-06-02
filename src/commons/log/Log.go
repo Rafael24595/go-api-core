90df8ee8853609e93f0fa20d138417555471f1ef
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-06-02 22:38:36 +0200

    [FEATURE] Remove logger console:
    - Add logger module with console and file modules.
    - update pre-commit-build.yml

--- src/commons/log/Log.go
+++ src/commons/log/Log.go
@@ -1,5 +1,11 @@
 package log
 
+import (
+	"strings"
+
+	"github.com/Rafael24595/go-api-core/src/commons/utils"
+)
+
 type Log interface {
 	Name() string
 	Records() []Record
@@ -18,9 +24,64 @@ type Log interface {
 var log Log = defaultLog()
 
 func defaultLog() Log {
-	log := newConsole(&Formatter{})
-	log.Messagef("Default logging is configured to use the %s instance", log.Name())
+	log := newloggerModule()
+	log.pushModule(newModuleConsole())
+	log.Messagef("Default logging is configured to use the %s instance with modules: %s", log.Name(), MODULE_CONSOLE)
 	return log
+}
+
+func ConfigureLog(session string, kargs map[string]utils.Any) Log {
+	code, ok := kargs["GO_LOG_INSTANCE"]
+	if !ok {
+		return log
+	}
+
+	if codeStr, _ := code.String(); codeStr == CODE_LOGGER_MODULE {
+		log = instanceModuleLogger(session, kargs)
+	}
+
+	return log
+}
+
+func instanceModuleLogger(session string, kargs map[string]utils.Any) Log {
+	modulesInterface, ok := kargs["GO_LOG_MODULES"]
+	if !ok {
+		return log
+	}
+
+	modulesStr, _ := modulesInterface.String()
+	modules := strings.Split(modulesStr, "|") 
+
+	newInstance := newloggerModule()
+	loaded := make(map[string]int)
+
+	for _, v := range modules {
+		switch v {
+		case MODULE_FILE:
+			if _, ok := loaded[v]; ok {
+				continue
+			}
+			newInstance.pushModule(newModuleFile(session))
+			loaded[MODULE_FILE] = 1
+		case MODULE_CONSOLE:
+			if _, ok := loaded[v]; ok {
+				continue
+			}
+			newInstance.pushModule(newModuleConsole())
+			loaded[MODULE_CONSOLE] = 1
+		default:
+			log.Panicf("The logger module %s is not found", v)
+		}
+	}
+
+	keys := make([]string, 0, len(loaded))
+	for k := range loaded {
+		keys = append(keys, k)
+	}
+
+	newInstance.Messagef("The logging is configured to use the %s instance with modules: %s", newInstance.Name(), strings.Join(keys, ", "))
+
+	return newInstance
 }
 
 func Name() string {
---
+++ src/commons/log/ModuleFile.go
@@ -0,0 +1,67 @@
+package log
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/Rafael24595/go-api-core/src/commons/routine"
+	"github.com/Rafael24595/go-api-core/src/infrastructure/repository/utils"
+)
+
+const MODULE_FILE = "FILE"
+const LOG_PATH = "./.logs"
+
+type moduleFile struct {
+	filePath  string
+	session   string
+	pool      *routine.StreamPool[[]Record]
+	formatter Formatter
+}
+
+func newModuleFile(session string) *moduleFile {
+	pool := routine.SyncStreamPool[[]Record](100).
+		EnableAutoDrain().
+		Make()
+	return &moduleFile{
+		filePath:  LOG_PATH,
+		session:   session,
+		pool:      pool,
+		formatter: Formatter{},
+	}
+}
+
+func (m *moduleFile) Vector(records []Record) []Record {
+	result := m.pool.Submit(func(ctx context.Context) ([]Record, error) {
+		return m.write(records)
+	})
+
+	if !result {
+		fmt.Println("Cannot write file pool buffer is full")
+	}
+
+	return records
+}
+
+func (m *moduleFile) Record(record *Record, throwPanic bool) *Record {
+	if throwPanic {
+		message := m.formatter.Format(*record)
+		panic(message)
+	}
+	return record
+}
+
+func (m *moduleFile) write(records []Record) ([]Record, error) {
+	jsonData, err := json.MarshalIndent(records, "", "  ") 
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	path := fmt.Sprintf("%s/%s.json", m.filePath, m.session)
+	err = utils.WriteFile(path, string(jsonData))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return records, nil
+}
