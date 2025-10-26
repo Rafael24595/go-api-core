371ba2f654e9cee3739dc2b1159a1211dbd1b0d8
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-10-26 22:04:53 +0100

    [FEATURE] Add tokes field to session.

--- src/commons/session/Session.go
+++ src/commons/session/Session.go
@@ -1,16 +1,17 @@
 package session
 
 type Session struct {
-	Username    string `json:"username"`
-	Secret      []byte `json:"secret"`
-	Timestamp   int64  `json:"timestamp"`
-	History     string `json:"history"`
-	Collection  string `json:"collection"`
-	Group       string `json:"group"`
-	IsProtected bool   `json:"is_protected"`
-	IsAdmin     bool   `json:"is_admin"`
-	Count       int    `json:"count"`
-	Refresh     string `json:"refresh"`
+	Username    string   `json:"username"`
+	Secret      []byte   `json:"secret"`
+	Timestamp   int64    `json:"timestamp"`
+	History     string   `json:"history"`
+	Collection  string   `json:"collection"`
+	Group       string   `json:"group"`
+	IsProtected bool     `json:"is_protected"`
+	IsAdmin     bool     `json:"is_admin"`
+	Count       int      `json:"count"`
+	Refresh     string   `json:"refresh"`
+	Tokens      []string `json:"tokens"`
 }
 
 func (s Session) IsVerified() bool {
