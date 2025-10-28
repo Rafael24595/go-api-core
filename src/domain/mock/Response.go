a66cc92809eec2e9ffa387b2437b7bbb5a3d70cc
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-10-29 00:59:17 +0100

    [FEATURE] Update Response definition.

--- src/domain/mock/Response.go
+++ src/domain/mock/Response.go
@@ -1,12 +1,7 @@
 package mock
 
-import (
-	"github.com/Rafael24595/go-api-core/src/domain/action/body"
-	"github.com/Rafael24595/go-api-core/src/domain/action/header"
-)
-
 type Response struct {
-	Status  int16             `json:"status"`
-	Headers header.Headers    `json:"headers"`
-	Body    body.BodyResponse `json:"body"`
+	Status  int               `json:"status"`
+	Headers map[string]string `json:"headers"`
+	Body    string            `json:"body"`
 }
