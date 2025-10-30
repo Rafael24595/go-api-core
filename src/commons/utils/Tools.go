3f31598b8b630859766584284b800b58ca8767f7
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-10-30 19:33:39 +0100

    [FEATURE] Add split command utility.

--- src/commons/utils/Tools.go
+++ src/commons/utils/Tools.go
@@ -1,6 +1,9 @@
 package utils
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 const ESCAPE_RUNE = '\\'
 
@@ -46,4 +49,60 @@ func SplitByRune(s string, sep rune) []string {
 	}
 
 	return append(fragments, string(buffer))
+}
+
+func SplitCommand(input string) ([]string, error) {
+	args := make([]string, 0)
+	buffer := make([]rune, 0)
+
+	quote := rune(0)
+	escaped := false
+	closed := false
+	
+	for _, r := range input {
+		switch {
+		case escaped:
+			buffer = append(buffer, r)
+			escaped = false
+			closed = false
+
+		case r == '\\':
+			escaped = true
+			closed = false
+
+		case quote != 0:
+			if r == quote {
+				quote = 0
+				closed = true
+			} else {
+				buffer = append(buffer, r)
+				closed = false
+			}
+
+		case r == '"' || r == '\'':
+			quote = r
+			closed = false
+
+		case r == ' ' || r == '\t' || r == '\n':
+			if len(buffer) > 0 || closed {
+				args = append(args, string(buffer))
+				buffer = make([]rune, 0)
+			}
+			closed = false
+
+		default:
+			buffer = append(buffer, r)
+			closed = false
+		}
+	}
+
+	if len(buffer) > 0 || closed {
+		args = append(args, string(buffer))
+	}
+
+	if quote != 0 {
+		return nil, fmt.Errorf("unclosed quote")
+	}
+
+	return args, nil
 }
