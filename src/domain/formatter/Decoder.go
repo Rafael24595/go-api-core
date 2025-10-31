7dbbcffdb6fb18072467b05a9efdf12afc13d562
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-10-31 01:29:58 +0100

    [FEATURE] Add curl decoder.

---
+++ src/domain/formatter/Decoder.go
@@ -0,0 +1,195 @@
+package formatter
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/Rafael24595/go-api-core/src/commons/utils"
+	"github.com/Rafael24595/go-api-core/src/domain"
+	"github.com/Rafael24595/go-api-core/src/domain/action"
+	auth_strategy "github.com/Rafael24595/go-api-core/src/domain/action/auth/strategy"
+	"github.com/Rafael24595/go-api-core/src/domain/action/body"
+	body_strategy "github.com/Rafael24595/go-api-core/src/domain/action/body/strategy"
+	"github.com/Rafael24595/go-api-core/src/domain/action/query"
+	"github.com/Rafael24595/go-collections/collection"
+)
+
+type curlData struct {
+	uri    string
+	tuples []tuple
+}
+
+type tuple struct {
+	flag string
+	data string
+}
+
+func CurlToRequest(curl []byte) (*action.Request, error) {
+	data, err := unmarshal(curl)
+	if err != nil {
+		return nil, err
+	}
+
+	uri, query := processUri(data.uri)
+
+	request := action.NewRequest("curl import", domain.GET, uri)
+	request.Query = *query
+
+	for _, v := range data.tuples {
+		switch v.flag {
+		case "-X":
+			request.Method = domain.HttpMethod(v.data)
+		case "-H", "--header":
+			request = processHeader(v.data, request)
+		case "-d", "--data", "--data-raw":
+			request.Body = *body_strategy.DocumentBody(true, body.Text, v.data)
+			if request.Method == domain.GET {
+				request.Method = domain.POST
+			}
+		case "--data-binary":
+			//TODO: Implement case.
+		case "-F", "--form":
+			//TODO: Implement case.
+		case "-u", "--user":
+			request = processBasicAuth(v.data, request)
+		case "-b", "--cookie":
+			request = processCookie(v.data, request)
+		}
+	}
+
+	return request, nil
+}
+
+func processUri(uri string) (string, *query.Queries) {
+	queries := query.NewQueries()
+
+	fragments := strings.SplitN(uri, "?", 2)
+	if len(fragments) == 1 {
+		return uri, queries
+	}
+
+	values, err := url.ParseQuery(fragments[1])
+	if err != nil {
+		return uri, queries
+	}
+
+	for key, vals := range values {
+		for _, v := range vals {
+			queries.Add(key, v)
+		}
+	}
+
+	return fragments[0], queries
+}
+
+func processHeader(data string, request *action.Request) *action.Request {
+	parts := strings.SplitN(data, ":", 2)
+	if len(parts) == 2 {
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+		request.Header.Add(key, value)
+	}
+	return request
+}
+
+func processBasicAuth(data string, request *action.Request) *action.Request {
+	parts := strings.SplitN(data, ":", 2)
+	user := strings.TrimSpace(parts[0])
+
+	pass := ""
+	if len(parts) > 1 {
+		pass = strings.TrimSpace(parts[1])
+	}
+
+	auth := auth_strategy.BasicAuth(true, user, pass)
+	request.Auth.PutAuth(*auth)
+	return request
+}
+
+func processCookie(data string, request *action.Request) *action.Request {
+	for hs := range strings.SplitSeq(data, ";") {
+		parts := strings.SplitN(hs, "=", 2)
+		if len(parts) == 2 {
+			key := strings.TrimSpace(parts[0])
+			value := strings.TrimSpace(parts[1])
+			request.Cookie.Put(key, value)
+		}
+	}
+	return request
+}
+
+func unmarshal(curl []byte) (*curlData, error) {
+	args, err := utils.SplitCommand(string(curl))
+	if err != nil {
+		return nil, err
+	}
+
+	uri := ""
+	tuples := make([]tuple, 0)
+
+	fragmensts := collection.VectorFromList(args)
+	head, ok := fragmensts.Shift()
+	if !ok || *head != "curl" {
+		return nil, errors.New("the command is not a valid curl sentence")
+	}
+
+	for fragmensts.Size() > 0 {
+		flag, ok := fragmensts.Shift()
+		if !ok {
+			return nil, errors.New("the command flag could not be empty")
+		}
+
+		url, ok, err := findUri(*flag, fragmensts)
+		if err != nil {
+			return nil, err
+		}
+
+		if ok {
+			uri = url
+			continue
+		}
+
+		data, ok := fragmensts.Shift()
+		if !ok {
+			return nil, errors.New("the command flag data could not be empty")
+		}
+
+		tuples = append(tuples, tuple{
+			flag: *flag,
+			data: *data,
+		})
+	}
+
+	return &curlData{
+		uri:    uri,
+		tuples: tuples,
+	}, nil
+}
+
+func findUri(flag string, fragmensts *collection.Vector[string]) (string, bool, error) {
+	if flag == "--uri" || flag == "--url" {
+		uri, ok := fragmensts.Shift()
+		if !ok {
+			return "", false, errors.New("the uri is not defined")
+		}
+
+		flag = *uri
+	} else if strings.HasPrefix(flag, "-") {
+		return "", false, nil
+	} else if !strings.Contains(flag, "://") && !strings.HasPrefix(flag, "/") {
+		return "", false, nil
+	}
+
+	u, err := url.Parse(flag)
+	if err != nil {
+		return "", false, err
+	}
+
+	if u.Scheme == "" && !strings.HasPrefix(flag, "/") {
+		return "", false, fmt.Errorf("invalid URI: %s", flag)
+	}
+
+	return flag, true, nil
+}
