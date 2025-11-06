dcdf61206bbe073e374782eb93e6ff6f1525b937
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-11-07 00:59:24 +0100

    [FEATURE] Improve snapshot cmd codebase.

--- src/commons/command/Snapshot.go
+++ src/commons/command/Snapshot.go
@@ -7,6 +7,7 @@ import (
 
 	command_helper "github.com/Rafael24595/go-api-core/src/commons/command/helper"
 	"github.com/Rafael24595/go-api-core/src/commons/configuration"
+	"github.com/Rafael24595/go-api-core/src/commons/format"
 	"github.com/Rafael24595/go-api-core/src/commons/system"
 	"github.com/Rafael24595/go-api-core/src/commons/utils"
 	"github.com/Rafael24595/go-api-core/src/infrastructure/repository"
@@ -157,42 +158,9 @@ func runSnapshotListeners(verbose bool) string {
 }
 
 func runSnapshotDetails(flag string, cmd *collection.Vector[string]) (string, error) {
-	value, ok := cmd.Shift()
-	if !ok {
-		return "", nil
-	}
-
-	tpc, typ, ok := strings.Cut(*value, "=")
-	if !ok {
-		return "", fmt.Errorf("invalid flag %q value %q", flag, *value)
-	}
-
-	topic, ok := system.TopicSnapshotFromString(tpc)
-	if !ok {
-		return "", fmt.Errorf("unknown topic: %s", *value)
-	}
-
-	config := configuration.Instance()
-	raw := config.EventHub.Topics(repository.SnapshotListener)
-	listeners := system.FilterTopicSnapshot(raw)
-
-	exists := collection.VectorFromList(listeners).
-		Contains(func(t system.TopicSnapshot) bool {
-			return t == topic
-		})
-
-	if !exists {
-		return "- no snapshots found -", nil
-	}
-
-	path := ""
-	switch typ {
-	case "csvt":
-		path = topic.CsvtPath()
-	}
-
-	if path == "" {
-		return "", fmt.Errorf("undefined path for type %q", typ)
+	path, err := findSnapshotDetailsPath(flag, cmd)
+	if err != nil {
+		return "", err
 	}
 
 	files, err := command_helper.FindSnapshots(path)
@@ -205,4 +173,33 @@ func runSnapshotDetails(flag string, cmd *collection.Vector[string]) (string, er
 	})
 
 	return strings.Join(snapshots.Collect(), "\n"), nil
+}
+
+func findSnapshotDetailsPath(flag string, cmd *collection.Vector[string]) (string, error) {
+	value, ok := cmd.Shift()
+	if !ok {
+		return "", nil
+	}
+
+	tpc, ext, ok := strings.Cut(*value, "=")
+	if !ok {
+		return "", fmt.Errorf("invalid flag %q value %q", flag, *value)
+	}
+
+	topic, ok := system.TopicSnapshotFromString(tpc)
+	if !ok {
+		return "", fmt.Errorf("unknown topic: %s", *value)
+	}
+
+	format, ok := format.DataFormatFromExtension(ext)
+	if !ok {
+		return "", fmt.Errorf("undefined format for extension %q", ext)
+	}
+
+	path, ok := topic.Path(format)
+	if !ok {
+		return "", fmt.Errorf("undefined path for extension %q", ext)
+	}
+
+	return path, nil
 }
