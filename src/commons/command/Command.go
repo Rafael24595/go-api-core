af6be1d750799fa9d8e18fd4980b6e610a0ee512
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-11-07 18:38:38 +0100

    [FEATURE] Add help action to cmd.

--- src/commons/command/Command.go
+++ src/commons/command/Command.go
@@ -2,14 +2,46 @@ package command
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/Rafael24595/go-api-core/src/commons/utils"
 	"github.com/Rafael24595/go-collections/collection"
 )
 
 const (
+	CMD      = "cmd"
 	SNAPSHOT = "snpsh"
 )
+
+const (
+	FLAG_CMD_HELP = "-h"
+)
+
+type commandAction struct {
+	Flag        string
+	Name        string
+	Description string
+	Example     string
+}
+
+var cmdHelp = commandAction{
+	Flag:        FLAG_CMD_HELP,
+	Name:        "Help",
+	Description: "Shows this help message.",
+	Example:     `cmd -h`,
+}
+
+var cmdSnapshot = commandAction{
+	Flag:        SNAPSHOT,
+	Name:        "Snapshot",
+	Description: "Manages in-memory persistence snapshots",
+	Example:     "snpsh -h",
+}
+
+var cmdActions = []commandAction{
+	cmdHelp,
+	cmdSnapshot,
+}
 
 func Exec(command string) (string, error) {
 	raw, err := utils.SplitCommand(command)
@@ -29,8 +61,45 @@ func Exec(command string) (string, error) {
 
 func run(head string, cmd *collection.Vector[string]) (string, error) {
 	switch head {
+	case CMD:
+		return root(cmd)
 	case SNAPSHOT:
 		return snapshot(cmd)
 	}
 	return "", fmt.Errorf("unknown command %q", head)
+}
+
+func root(cmd *collection.Vector[string]) (string, error) {
+	if cmd.Size() == 0 {
+		return runCmdHelp(), nil
+	}
+
+	for cmd.Size() > 0 {
+		flag, ok := cmd.Shift()
+		if !ok {
+			break
+		}
+
+		switch *flag {
+		case FLAG_CMD_HELP:
+			return runCmdHelp(), nil
+		}
+	}
+
+	return "", nil
+}
+
+func runCmdHelp() string {
+	title := "Available cmd applications:\n"
+	return runHelp(title, cmdActions)
+}
+
+func runHelp(title string, actions []commandAction) string {
+	result := make([]string, 0)
+	result = append(result, title)
+	for _, a := range actions {
+		result = append(result, fmt.Sprintf(" %s: %s", a.Flag, a.Description))
+		result = append(result, fmt.Sprintf("  Example: %s\n", a.Example))
+	}
+	return strings.Join(result, "\n")
 }
