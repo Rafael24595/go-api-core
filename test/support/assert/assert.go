13ebb1da885162aa237dd528088f78987c03887f
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-11-13 02:18:50 +0100

    [FEATURE] Add Error, NoError and Len to assert package.

--- test/support/assert/assert.go
+++ test/support/assert/assert.go
@@ -59,3 +59,57 @@ func LessOrEqual[T cmp.Ordered](t *testing.T, want, have T, message ...any) {
 
 	t.Errorf("Expected less or equal than %v, but got %v", want, have)
 }
+
+func Error(t *testing.T, err error, message ...any) {
+	t.Helper()
+
+	if err != nil {
+		return
+	}
+
+	if len(message) > 0 {
+		if format, ok := message[0].(string); ok {
+			msg := fmt.Sprintf(format, message[1:]...)
+			t.Errorf("%s. Error expected but nothing found", msg)
+			return
+		}
+	}
+
+	t.Error("Expected error found but nothing found")
+}
+
+func NoError(t *testing.T, err error, message ...any) {
+	t.Helper()
+
+	if err == nil {
+		return
+	}
+
+	if len(message) > 0 {
+		if format, ok := message[0].(string); ok {
+			msg := fmt.Sprintf(format, message[1:]...)
+			t.Errorf("%s. Unexpected error: '%s'", msg, err.Error())
+			return
+		}
+	}
+
+	t.Errorf("Unexpected error found: '%s'", err.Error())
+}
+
+func Len[T any](t *testing.T, want int, have []T, message ...any) {
+	t.Helper()
+
+	if want == len(have) {
+		return
+	}
+
+	if len(message) > 0 {
+		if format, ok := message[0].(string); ok {
+			msg := fmt.Sprintf(format, message[1:]...)
+			t.Errorf("%s — expected: %v, got: %v", msg, want, have)
+			return
+		}
+	}
+
+	t.Errorf("Expected %v, but got %v", want, have)
+}
